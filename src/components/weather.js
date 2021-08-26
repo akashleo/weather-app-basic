b08d606d5767d1a30c76401fc40bb943ddf70838
Akash <akashleo2009@gmail.com>
2021-08-27 03:20:02 +0530

    added icon/centered card/added background

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,43 +1,57 @@
 import React from "react";
-//import "./styles.css";
+//import mystyles from "./styles.css";
 import { Card, Image, Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import moment from "moment";
 
-const CardExampleCard = ({ weatherData }) => (
-  <Card>
-    <Image
-      src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-      wrapped
-      ui={false}
-    />
-    <Card.Content>
-      <Card.Header>
-        <Icon name="map marker alternate" />
-        {weatherData.name}
-      </Card.Header>
-      <Card.Description>
-        <p>
-          Sunrise:{" "}
-          {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
-        </p>
-        <p>
-          Sunset:{" "}
-          {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-IN")}
-        </p>
-        <p>Description: {weatherData.weather[0].main}</p>
-        <p>Humidity: {weatherData.main.humidity} %</p>
-        <p>Day: {moment().format("dddd")}</p>
-        <p>Date: {moment().format("LL")}</p>
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <Icon name="tint" />
-      {weatherData.main.humidity} % &nbsp;&nbsp;
-      <Icon name="thermometer" />
-      {weatherData.main.temp} &deg;C
-    </Card.Content>
-  </Card>
-);
+const CardExampleCard = ({ weatherData }) => {
+  const icon = weatherData.weather[0].icon;
+  const srcSTR = "http://openweathermap.org/img/w/" + icon + ".png";
+
+  const style = {
+    margin: "auto",
+    marginTop: "5rem",
+  };
+  return (
+    <Card style={style}>
+      <Image
+        //src="https://store-images.s-microsoft.com/image/apps.10595.14397430983184912.cfdf6f70-0a34-4999-b494-936559d822c3.7355576f-baf9-4be3-8b34-27bdc6ac1bd2"
+        src={srcSTR}
+        wrapped
+        ui="false"
+      />
+      <Card.Content>
+        <Card.Header>
+          <Icon name="map marker alternate" />
+          {weatherData.name}
+        </Card.Header>
+        <Card.Description>
+          <p>
+            Sunrise:{" "}
+            {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
+              "en-IN"
+            )}
+          </p>
+          <p>
+            Sunset:{" "}
+            {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
+              "en-IN"
+            )}
+          </p>
+          <p>Description: {weatherData.weather[0].main}</p>
+          <p>Humidity: {weatherData.main.humidity} %</p>
+          <p>Day: {moment().format("dddd")}</p>
+          <p>Date: {moment().format("LL")}</p>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="tint" />
+        {weatherData.main.humidity} % &nbsp;&nbsp;
+        <Icon name="thermometer" />
+        {weatherData.main.temp} &deg;C
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default CardExampleCard;
